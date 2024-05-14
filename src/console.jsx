c0d13c67bc6e24613b7d4ebbd4b1ef030b41e84b
viktorK2007 <vitya.krupnin.37125@gmail.com>
2024-05-14 20:52:15 +0300

    ыефке

---
+++ src/console.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+function App() {
+  return (
+    <div className='background' style={styles.background} >
+      
+    </div>
+  );
+}
+const styles ={
+  background:{
+    width: '100vw',
+    height: `100vh`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
+} 
+export default App;
